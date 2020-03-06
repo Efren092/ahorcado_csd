706a4e727ff6b4aa08dff56235e824a7862ccb8d
Patricio <gablake8@hotmail.com>
2020-03-06 11:01:14 -0500

    add tdd for validate ahorcado instance

--- lib/ahorcado.rb
+++ lib/ahorcado.rb
@@ -1,7 +1,12 @@
 class Ahorcado
-    def initialize palabra= 'ahorcado'
+    def initialize palabra = 'ahorcado'
         @palabra = palabra
     end
+
+    def obtener_palabra
+        @palabra
+    end
+
     def pintar_lineas
     if @palabra == 'ahorcado' 
         '_ _ _ _ _ _ _ _'
--- spec/ahorcado_spec.rb
+++ spec/ahorcado_spec.rb
@@ -1,6 +1,18 @@
 require './lib/ahorcado.rb'
 
 describe Ahorcado do
+    it 'si no se define una palabra la palabra por defecto es ahorcado' do
+        ahorcado = Ahorcado.new
+        resultado = ahorcado.obtener_palabra
+        expect(resultado).to eq 'ahorcado'
+    end
+
+    it 'cuando ingreso la palabra corto la palbra a adivinar es corto' do
+        ahorcado = Ahorcado.new 'corto'
+        resultado = ahorcado.obtener_palabra
+        expect(resultado).to eq 'corto'
+    end
+
     it 'La palabra ahorcado debe pintar 8 lineas' do 
         ahorcado = Ahorcado.new 'ahorcado'
         result = ahorcado.pintar_lineas
