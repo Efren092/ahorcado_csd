7520166d0de857862875e73e1fd22c03b3b5cef1
Efren Narvaez <efren.narvaez@tipti.market>
2020-03-06 10:45:06 -0500

    include lines at index

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require './config'
+require './lib/ahorcado.rb'
 
 get '/' do
-erb(:index)
+    ahorcado = Ahorcado.new 'ahorcado'
+    lineas = ahorcado.pintar_lineas
+    session['lineas'] = lineas
+    erb(:index)
 end
