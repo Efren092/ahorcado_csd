cdd46e6b56c3345384423c7e3e746e07a0378369
Efren Narvaez <efren.narvaez@tipti.market>
2020-03-06 14:36:01 -0500

    persistir variables

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require './config'
 require './lib/ahorcado.rb'
 
 get '/' do
-    ahorcado = Ahorcado.new 'ahorcado'
-    lineas = ahorcado.pintar_lineas
+    session['ahorcado'] = Ahorcado.new 'ahorcado'
+    lineas = session['ahorcado'].pintar_lineas
     session['lineas'] = lineas
     erb(:index)
 end
 
 post '/validar' do
-    ahorcado = Ahorcado.new 'ahorcado'
+    ahorcado = session['ahorcado']
     letra = params['intento']
     linea_y_letras = ahorcado.pintar_letra letra
     session['lineas'] = linea_y_letras
